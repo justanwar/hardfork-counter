d8b7f20e299fcc00faa43aa838e93cd649f46bfd
justanwar <anwarzapari@gmail.com>
2025-10-27 21:24:19 +0800

    Write to temporary file

--- hfcounter.sh
+++ hfcounter.sh
@@ -12,6 +12,10 @@
 #    exit
 #fi
 
+# Create temp file. Clear if exist
+touch temp.txt
+> temp.txt
+
 # Clear html
 > index.html
 
@@ -26,21 +30,21 @@ dist_sn=$(($hf_block_sn-$curr_block))
 dist_lela=$(($LELANTUS_GRACEFUL_PERIOD-$curr_block))
 
 if [ $dist_sn -le 0 ]; then
-    echo "Spark Name hard fork block reached."
+    echo "Spark Name hard fork block reached." >> temp.txt
 else
-    echo "Spark Name hard fork block is $hf_block_sn, $dist_sn blocks to go."
+    echo "Spark Name hard fork block is $hf_block_sn, $dist_sn blocks to go." >> temp.txt
     # Assuming average of 150 seconds per block (2.5 minutes)
     seconds=$((dist_sn*150))
 
-    date -u -d @${seconds} +"In $(($seconds/3600/24)) days %H hours %M minutes."
+    date -u -d @${seconds} +"In $(($seconds/3600/24)) days %H hours %M minutes." >> temp.txt
  
 fi
 
 if [ $dist_lela -le 0 ]; then
-    echo "Lelantus redemption period ended."
+    echo "Lelantus redemption period ended." >> temp.txt
 else
-    echo "Lelantus redemption ends at $LELANTUS_GRACEFUL_PERIOD, $dist_lela blocks to go."
+    echo "Lelantus redemption ends at $LELANTUS_GRACEFUL_PERIOD, $dist_lela blocks to go." >> temp.txt
     seconds_lela=$((dist_lela*150))
 
-    date -u -d @${seconds_lela} +"In $(($seconds_lela/3600/24)) days %H hours %M minutes."
+    date -u -d @${seconds_lela} +"In $(($seconds_lela/3600/24)) days %H hours %M minutes." >> temp.txt
 fi
